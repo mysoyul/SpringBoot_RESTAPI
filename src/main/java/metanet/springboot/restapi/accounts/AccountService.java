d1b7c399e4b214d7d0d516da65fbc3ae64a2c00e
vega2k <vega2k@naver.com>
2021-12-14 15:42:54 +0900

    AccountAdapter add

--- src/main/java/metanet/springboot/restapi/accounts/AccountService.java
+++ src/main/java/metanet/springboot/restapi/accounts/AccountService.java
@@ -1,18 +1,11 @@
 package metanet.springboot.restapi.accounts;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,12 +21,8 @@ public class AccountService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
-        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+        //return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+        return new AccountAdapter(account);
     }
 
-    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
-        return roles.stream() //Stream<AccountRole>
-                .map(role -> new SimpleGrantedAuthority(role.name()))  //Stream<SimpleGrantedAuthority>
-                .collect(Collectors.toSet()); //Collection<GrantedAuthority>
-    }
 }
