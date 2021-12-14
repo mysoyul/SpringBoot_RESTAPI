c805055764ab270c9ad5b16f69566d666a6fd539
vega2k <vega2k@naver.com>
2021-12-14 14:43:53 +0900

    AuthServerConfig add

--- src/main/java/metanet/springboot/restapi/config/AuthServerConfig.java
+++ src/main/java/metanet/springboot/restapi/config/AuthServerConfig.java
@@ -3,6 +3,7 @@ package metanet.springboot.restapi.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
@@ -16,6 +17,23 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                //client_id
+                .withClient("myApp")
+                //client_secret
+                .secret(this.passwordEncoder.encode("pass"))
+                //password - password cridential grant 방식, refresh_token - access token 갱신할때
+                .authorizedGrantTypes("password", "refresh_token")
+                //접근범위
+                .scopes("read", "write")
+                //access token 만료시간 600초 이므로 10분
+                .accessTokenValiditySeconds(10 * 60)
+                //refresh token 만료시간 3600초 이므로 1시간
+                .refreshTokenValiditySeconds(6 * 10 * 60);
     }
 
 
